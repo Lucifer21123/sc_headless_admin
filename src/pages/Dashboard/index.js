cc995e19f6b7705aae279bfa29b760c9dbd195c3
ironfox <cooldev0127@gmail.com>
2021-05-23 16:59:03 -0400

    index page display fix

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,21 +1,21 @@
-import React from 'react';
+import React from "react";
 import PageLayout from "../../components/PageLayout";
 import { Route, Switch, useRouteMatch } from "react-router-dom";
 import AdminDashboard from "../AdminDashboard";
-import AdminDashboardEmpty from '../AdminDashboardEmpty';
-import AlertPage from '../AdminAlerts';
+import AdminDashboardEmpty from "../AdminDashboardEmpty";
+import AlertPage from "../AdminAlerts";
 
 const Dashboard = () => {
-    let { path, url } = useRouteMatch();
+  let { path, url } = useRouteMatch();
   return (
     <div className="admin-default dashboard">
-        <PageLayout>
-            <Switch>
-                <Route exact path={path} component={AdminDashboardEmpty} />
-                <Route path={`${path}/main`} component={AdminDashboard} />
-                <Route path={`${path}/alerts`} component={AlertPage} />
-            </Switch>
-        </PageLayout>
+      <PageLayout>
+        <Switch>
+          <Route exact path={path} component={AdminDashboard} />
+          <Route path={`${path}/main`} component={AdminDashboardEmpty} />
+          <Route path={`${path}/alerts`} component={AlertPage} />
+        </Switch>
+      </PageLayout>
     </div>
   );
 };
