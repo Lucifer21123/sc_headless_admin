0a1b1669b64933104504a93de4d9ecf618867018
ironfox <cooldev0127@gmail.com>
2021-05-24 22:48:42 -0400

    AdminAlerts page some bug fix, AdminNoAlert page made

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -4,6 +4,7 @@ import { Route, Switch, useRouteMatch } from "react-router-dom";
 import AdminDashboard from "../AdminDashboard";
 import AdminDashboardEmpty from "../AdminDashboardEmpty";
 import AlertPage from "../AdminAlerts";
+import AlertNoPage from "../AdminNoAlerts";
 
 const Dashboard = () => {
   let { path, url } = useRouteMatch();
@@ -14,6 +15,7 @@ const Dashboard = () => {
           <Route exact path={path} component={AdminDashboardEmpty} />
           <Route path={`${path}/main`} component={AdminDashboard} />
           <Route path={`${path}/alerts`} component={AlertPage} />
+          <Route path={`${path}/alerts-no`} component={AlertNoPage} />
         </Switch>
       </PageLayout>
     </div>
