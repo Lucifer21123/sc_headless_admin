e2201d30571cf903a52c6049a2067f77cf47695a
ironfox <cooldev0127@gmail.com>
2021-05-26 21:46:50 -0400

    aa

--- src/data/sidebar/sidebarInfo.ts
+++ src/data/sidebar/sidebarInfo.ts
@@ -3,7 +3,7 @@ import React from 'react';
 export const sidebarInfo = [
     {
       id: 1,
-      href: "/dashboard",
+      href: "/dashboard/main",
       img: "icon icon-dashboard",
       content: "Dashboard",
       num: "",
